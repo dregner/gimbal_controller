76f6658271e89c67cbd0b61fd2e6154f5d41139f
daniel.regner <daniel.regner@grad.ufsc.br>
2020-05-18 17:25:38 -0300

    Inverse kinematic working fine

--- src/gimbal/inverse_kinematic.cpp
+++ src/gimbal/inverse_kinematic.cpp
@@ -36,6 +36,7 @@ int object_count;
 float deg2rad = M_PI / 180;
 float rad2deg = 180 / M_PI;
 float pixel_dimenson = 3.4; //Pixel dimension of X5S
+float L;
 int focal_length = 30; //Focal length of camera len
 int dx = 5; //Distence to object
 float GSD = dx * pixel_dimenson / focal_length;
@@ -47,15 +48,12 @@ int frequence = 60;
 // TODO: Ajustar parâmetros Kc e z0
 float error_px_y = 0, error_px_x = 0;
 float u_px_y = 0, u_px_x = 0;
-float u_px_y_k = u_px_y, u_px_x_k = u_px_x;
 float u_k_x = 0, u_k_y = 0;
 float er_k_x = 0, er_k_y = 0;
 
 /// Tempo de amostragem para malha de controle
-int Ts = 10;
+int Ts = 20;
 int countt = 0, tout = 0;
-int histerese_error_x = 5;
-int histerese_error_y = 5;
 bool first_time = true;
 
 /// Leitura Joint States
@@ -68,9 +66,6 @@ static std::ofstream states;
 
 class Inverse_Kinematic {
 private:
-
-
-    int frequence = 10;
 
     ros::NodeHandle nh_;
 
@@ -128,8 +123,9 @@ public:
         resolution_x = GetParams::getResolution_x();
         aov_h = GetParams::getAov_h();
         focal_length = (rad2deg * 2 * atan(resolution_x / 2 * pixel_dimenson / 1000)) / aov_h;
-        GSD = dx * pixel_dimenson / focal_length;
-
+        L = 2*tan(deg2rad*aov_h/2)*dx;
+//        GSD = dx * pixel_dimenson / focal_length;
+        GSD = 1000*L/resolution_x;
     }
 
     void roll_value(const control_msgs::JointControllerState::ConstPtr &msg) {
@@ -151,37 +147,74 @@ public:
 
     void read_px(const darknet_ros_msgs::BoundingBoxes::ConstPtr &msg) {
         object_id = msg->bounding_boxes[object_count].id;
-        if (object_id == 32) {
+        if (object_id == 49 || object_id == 32) {
             int xmin = msg->bounding_boxes[object_count].xmin;
             int xmax = msg->bounding_boxes[object_count].xmax;
             int ymin = msg->bounding_boxes[object_count].ymin;
             int ymax = msg->bounding_boxes[object_count].ymax;
-            pixel_x = (xmax - xmin) / 2 + xmin;
-            pixel_y = (ymax - ymin) / 2 + ymin;
-            ROS_INFO("n_object: %i px: %i, py: %i", object_count, pixel_x, pixel_y);
+            pixel_x = pixel_x*0.95 + 0.05*((xmax - xmin) / 2 + xmin);
+            pixel_y = pixel_y*0.95 + 0.05*((ymax - ymin) / 2 + ymin);
+            cout << "Camera Resolution" << endl;
+            cout << "\tResolution: " << resolution_x << " x " << resolution_y << "\tLx: " << L << "\tGSD: " << GSD/1000 << " m/px" << endl;
+            cout  << "Darknet detection" << endl;
+            cout << "\tn_object: " << object_count << "\tpx: " << pixel_x << "\tpy: " << pixel_y << endl;
         } else {
             object_count++;
             if (object_count > object_founded) {
                 object_count = 0;
-                ROS_INFO("NOT FOUNDED PROPER OBJECT");
+                cout << "NO FOUNDED PROPER OBJECT" << endl;
             }
         }
-        control();
-        ROS_INFO("resolution: %i x %i - GSD: %f", resolution_x, resolution_y, GSD);
-        ROS_INFO("r: %f, p: %f, y: %f", roll, pitch, yaw);
+//        control();
+        inverse_kinematic();
+
+
+//        ROS_INFO("resolution: %i x %i - GSD: %f", resolution_x, resolution_y, GSD);
+        cout << "Gimbal angles" << endl;
+        cout << fixed  << "\tr: " << round(roll) << "\tp: " << round(pitch) << "\ty: " << round(yaw) << endl;
+        cout << "\033[2J\033[1;1H";     // clear terminal
+
+    }
+
+    float round(float var)
+    {
+        // 37.66666 * 100 =3766.66
+        // 3766.66 + .5 =3767.16    for rounding off value
+        // then type cast to int so value is 3767
+        // then divided by 100 so the value converted into 37.67
+        float value = (int)(var * 1000+ .5);
+        return (float)value / 1000;
     }
 
 
     void inverse_kinematic() {
         float Zg = (pixel_y - central_pixel_y) * GSD / 1000; //(px - px)*1e-3*m/px/1e3
         float Yg = (-pixel_x + central_pixel_x) * GSD / 1000; //(px - px)*1e-3*m/px1e3
-        ROS_INFO("Yg: %f Zg: %f", Yg, Zg);
+        cout << "XYZ Gimbal Inverse Kinematic" <<endl;
+        cout << "\tYg: " << Yg <<"\tZg: " <<Zg << endl;
         pitch_ik = asin(Zg / dx); //Zg/abs(Zg)*
+        pitch_ik = round(pitch_ik);
         yaw_ik = asin(Yg / (dx * cos(pitch_ik))); //Yg/abs(Yg)*
-        ROS_INFO("Inverse");
-        ROS_INFO("yaw ik: %f", yaw_ik);
-        ROS_INFO("pitch ik: %f", pitch_ik);
+        yaw_ik = round(yaw_ik);
+        float pitch_total = round(pitch + pitch_ik);
+        float yaw_total = round(yaw + yaw_ik);
+//        control_ik(yaw_ik, pitch_ik);
+        cout << "Inverse" << endl;
+        cout << "\tyaw ik: " << yaw_ik << "\tyaw desired: " << yaw_total << endl;
+        cout << "\tpitch ik: " << pitch_ik << "\tpitch desired: " << pitch_total <<endl;
 
+    }
+
+    void control_ik(float yaw, float pitch) {
+        float ux, uy;
+        ux = u_k_x + yaw;
+        uy = u_k_y + pitch;
+        msg_yaw.data = ux;
+        pub_yaw.publish(msg_yaw);
+        msg_pitch.data = uy;
+        pub_pitch.publish(msg_pitch);
+        u_k_x = ux;
+        u_k_y = uy;
     }
 
 
@@ -189,11 +222,12 @@ public:
 //        ros::Rate loop_rate(this->frequence);
         float er_x = central_pixel_x - pixel_x;
         float er_y = central_pixel_y - pixel_y;
-        ROS_INFO("er_x: %f", er_x);
-        ROS_INFO("er_y: %f", er_y);
+        cout << "pixel error" << endl;
+        cout << "\ter_x: " <<er_x <<"\ter_y: " << er_y<< endl;
         er_x = er_x * GSD / 1000;
         er_y = er_y * GSD / 1000;
         if (countt > Ts) {
+//            inverse_kinematic();
             control_x_position(er_x);
             control_y_position(er_y);
             countt = 0;
