e83d1942fe02a764d0b9a1c9832e15c545dfa319
daniel.regner <daniel.regner@grad.ufsc.br>
2020-05-27 09:24:08 -0300

    Inverse Kinematic working fine

--- src/gimbal/inverse_kinematic.cpp
+++ src/gimbal/inverse_kinematic.cpp
@@ -36,7 +36,7 @@ int object_count;
 float deg2rad = M_PI / 180;
 float rad2deg = 180 / M_PI;
 float pixel_dimenson = 3.4; //Pixel dimension of X5S
-float L;
+float Lx;
 int focal_length = 30; //Focal length of camera len
 int dx = 5; //Distence to object
 float GSD = dx * pixel_dimenson / focal_length;
@@ -52,13 +52,13 @@ float u_k_x = 0, u_k_y = 0;
 float er_k_x = 0, er_k_y = 0;
 
 /// Tempo de amostragem para malha de controle
-int Ts = 20;
-int countt = 0, tout = 0;
+int Ts = 30;
+int countt = 0, counttt = 0, tout = 0;
 bool first_time = true;
 
 /// Leitura Joint States
 float roll, pitch, yaw;
-float yaw_ik, pitch_ik;
+float yaw_ik, pitch_ik, yaw_total, pitch_total;
 
 
 using namespace std;
@@ -77,13 +77,9 @@ private:
     ros::Subscriber sub_pitch;
     ros::Subscriber sub_yaw;
 
-
-    std_msgs::Float64 msg_roll;
     std_msgs::Float64 msg_pitch;
     std_msgs::Float64 msg_yaw;
 
-
-    ros::Publisher pub_roll;
     ros::Publisher pub_pitch;
     ros::Publisher pub_yaw;
 
@@ -91,8 +87,6 @@ private:
 public:
     Inverse_Kinematic() {
 
-        sub_found_object = nh_.subscribe("darknet_ros/found_object", 10, &Inverse_Kinematic::found_object, this);
-        sub_boundingboxes = nh_.subscribe("/darknet_ros/bounding_boxes", 10, &Inverse_Kinematic::read_px, this);
 
         check_parameters();
 
@@ -106,26 +100,36 @@ public:
                                 &Inverse_Kinematic::yaw_value,
                                 this);
 
+        sub_found_object = nh_.subscribe("darknet_ros/found_object", 10, &Inverse_Kinematic::found_object, this);
+        sub_boundingboxes = nh_.subscribe("/darknet_ros/bounding_boxes", 10, &Inverse_Kinematic::read_px, this);
+
         pub_pitch = nh_.advertise<std_msgs::Float64>("/" + GetParams::getRpaName() + "/gimbal_pitch_position/command",
                                                      100);
         pub_yaw = nh_.advertise<std_msgs::Float64>("/" + GetParams::getRpaName() + "/gimbal_yaw_position/command", 1);
 
 
-//        states.open("states.txt");
+        states.open("inverse.txt");
     }
 
     ~Inverse_Kinematic() {
 
     }
 
+    float round(float var) {
+        float value = (int) (var * 1000 + 0.5);
+        return (float) value / 1000;
+    }
+
     void check_parameters() {
         resolution_y = GetParams::getResolution_y();
         resolution_x = GetParams::getResolution_x();
+        central_pixel_x = resolution_x / 2;
+        central_pixel_y = resolution_y / 2;
         aov_h = GetParams::getAov_h();
-        focal_length = (rad2deg * 2 * atan(resolution_x / 2 * pixel_dimenson / 1000)) / aov_h;
-        L = 2*tan(deg2rad*aov_h/2)*dx;
+        focal_length = (int) (rad2deg * 2 * atan((float) resolution_x / 2 * pixel_dimenson / 1000)) / aov_h;
 //        GSD = dx * pixel_dimenson / focal_length;
-        GSD = 1000*L/resolution_x;
+        Lx = 2 * tan(deg2rad * (float) aov_h / 2) * (float) dx;
+        GSD = Lx / (float) resolution_x; // GSD from a gazebo environment where doesnt have a pixel dimension (m/px)
     }
 
     void roll_value(const control_msgs::JointControllerState::ConstPtr &msg) {
@@ -143,26 +147,33 @@ public:
 
     void found_object(const darknet_ros_msgs::ObjectCount::ConstPtr &msg) {
         object_founded = msg->count;
+        if(first_time){
+            pitch_total = pitch;
+            yaw_total = yaw;
+            first_time = false;
+        }
     }
 
     void read_px(const darknet_ros_msgs::BoundingBoxes::ConstPtr &msg) {
         object_id = msg->bounding_boxes[object_count].id;
-        if (object_id == 49 || object_id == 32) {
+        if (object_id == 49 || object_id == 32 || object_id == 29) {
             int xmin = msg->bounding_boxes[object_count].xmin;
             int xmax = msg->bounding_boxes[object_count].xmax;
             int ymin = msg->bounding_boxes[object_count].ymin;
             int ymax = msg->bounding_boxes[object_count].ymax;
-            pixel_x = pixel_x*0.95 + 0.05*((xmax - xmin) / 2 + xmin);
-            pixel_y = pixel_y*0.95 + 0.05*((ymax - ymin) / 2 + ymin);
+            pixel_x = pixel_x * 0.95 + 0.05 * ((xmax - xmin) / 2 + xmin);
+            pixel_y = pixel_y * 0.95 + 0.05 * ((ymax - ymin) / 2 + ymin);
             cout << "Camera Resolution" << endl;
-            cout << "\tResolution: " << resolution_x << " x " << resolution_y << "\tLx: " << L << "\tGSD: " << GSD/1000 << " m/px" << endl;
-            cout  << "Darknet detection" << endl;
+            cout << "\tResolution: " << resolution_x << " x " << resolution_y << "\tLx: " << Lx << "\tGSD: "
+                 << GSD << " m/px" << endl;
+            cout << "\tCentral pixel: " << central_pixel_x << " x " << central_pixel_y << endl;
+            cout << "Darknet detection" << endl;
             cout << "\tn_object: " << object_count << "\tpx: " << pixel_x << "\tpy: " << pixel_y << endl;
         } else {
             object_count++;
             if (object_count > object_founded) {
                 object_count = 0;
-                cout << "NO FOUNDED PROPER OBJECT" << endl;
+                cout << "NOT FOUND PROPER OBJECT" << endl;
             }
         }
 //        control();
@@ -171,50 +182,52 @@ public:
 
 //        ROS_INFO("resolution: %i x %i - GSD: %f", resolution_x, resolution_y, GSD);
         cout << "Gimbal angles" << endl;
-        cout << fixed  << "\tr: " << round(roll) << "\tp: " << round(pitch) << "\ty: " << round(yaw) << endl;
+        cout << fixed << "\tr: " << round(roll) << "\tp: " << round(pitch) << "\ty: " << round(yaw) << endl;
         cout << "\033[2J\033[1;1H";     // clear terminal
 
-    }
 
-    float round(float var)
-    {
-        // 37.66666 * 100 =3766.66
-        // 3766.66 + .5 =3767.16    for rounding off value
-        // then type cast to int so value is 3767
-        // then divided by 100 so the value converted into 37.67
-        float value = (int)(var * 1000+ .5);
-        return (float)value / 1000;
+
+
     }
 
 
     void inverse_kinematic() {
-        float Zg = (pixel_y - central_pixel_y) * GSD / 1000; //(px - px)*1e-3*m/px/1e3
-        float Yg = (-pixel_x + central_pixel_x) * GSD / 1000; //(px - px)*1e-3*m/px1e3
-        cout << "XYZ Gimbal Inverse Kinematic" <<endl;
-        cout << "\tYg: " << Yg <<"\tZg: " <<Zg << endl;
+        float Zg = (pixel_y - central_pixel_y) * GSD; //(px - px)*m/px
+        float Yg = (-pixel_x + central_pixel_x) * GSD; //(px - px)*m/px
+        cout << "XYZ Gimbal Inverse Kinematic" << endl;
+        cout << "\tYg: " << Yg << "\tZg: " << Zg << endl;
         pitch_ik = asin(Zg / dx); //Zg/abs(Zg)*
         pitch_ik = round(pitch_ik);
         yaw_ik = asin(Yg / (dx * cos(pitch_ik))); //Yg/abs(Yg)*
         yaw_ik = round(yaw_ik);
-        float pitch_total = round(pitch + pitch_ik);
-        float yaw_total = round(yaw + yaw_ik);
-//        control_ik(yaw_ik, pitch_ik);
+        pitch_total = 0.8*pitch_total + 0.2*round(pitch + pitch_ik);
+        yaw_total = 0.8*yaw_total + 0.2*round(yaw + yaw_ik);
+        control_ik(yaw_total, pitch_total);
         cout << "Inverse" << endl;
         cout << "\tyaw ik: " << yaw_ik << "\tyaw desired: " << yaw_total << endl;
-        cout << "\tpitch ik: " << pitch_ik << "\tpitch desired: " << pitch_total <<endl;
-
+        cout << "\tpitch ik: " << pitch_ik << "\tpitch desired: " << pitch_total << endl;
+        if (states.is_open() && counttt > Ts) {
+            states << tout << "\t" << pitch << "\t" << yaw << "\t" << pitch_ik + pitch << "\t" << yaw_ik + yaw << "\t"
+                   << Yg << "\t" << Zg << "\t"
+                   << pixel_x << "\t" << pixel_y << "\t" << central_pixel_x - pixel_x << "\t"
+                   << central_pixel_y - pixel_y
+                   << "\n";
+            counttt = 0;
+        }
+        counttt++;
     }
 
     void control_ik(float yaw, float pitch) {
         float ux, uy;
-        ux = u_k_x + yaw;
-        uy = u_k_y + pitch;
-        msg_yaw.data = ux;
-        pub_yaw.publish(msg_yaw);
-        msg_pitch.data = uy;
-        pub_pitch.publish(msg_pitch);
-        u_k_x = ux;
-        u_k_y = uy;
+        countt++;
+        tout++;
+        if (countt > Ts) {
+            msg_yaw.data = yaw;
+            pub_yaw.publish(msg_yaw);
+            msg_pitch.data = pitch;
+            pub_pitch.publish(msg_pitch);
+            countt = 0;
+        }
     }
 
 
@@ -223,9 +236,9 @@ public:
         float er_x = central_pixel_x - pixel_x;
         float er_y = central_pixel_y - pixel_y;
         cout << "pixel error" << endl;
-        cout << "\ter_x: " <<er_x <<"\ter_y: " << er_y<< endl;
-        er_x = er_x * GSD / 1000;
-        er_y = er_y * GSD / 1000;
+        cout << "\ter_x: " << er_x << "\ter_y: " << er_y << endl;
+        er_x = er_x * GSD;
+        er_y = er_y * GSD;
         if (countt > Ts) {
 //            inverse_kinematic();
             control_x_position(er_x);
@@ -235,20 +248,22 @@ public:
 //        loop_rate.sleep();
         er_k_y = er_y;
         er_k_x = er_x;
+        if (states.is_open()) {
+            states << tout << "\t" << pitch << "\t" << yaw << "\t" << u_k_x << "\t" << u_k_y << "\t"
+                   << central_pixel_x - pixel_x << "\t"
+                   << central_pixel_y - pixel_y << "\t" << pixel_x << "\t" << pixel_y << "\n";
+        }
         countt++;
         tout++;
 
-//        if (states.is_open() && tout < 2000) {
-//            states << tout << "\t" << yaw << "\t" << u_k_x << "\t" << er_x / GDS
-//                   << "\t" << pitch << "\t" << u_k_x << "\t" << er_y / GDS << "\n";
-//        }
+
     }
 
     void control_x_position(float er_x) {
 
         float u;
 
-        if (abs(er_x) > central_pixel_x * GSD / 1000) {
+        if (abs(er_x) > central_pixel_x * GSD) {
             u = u_k_x;
         } else {
             u = (0.10 * (er_k_x - 0.75 * er_x) + u_k_x);
@@ -261,7 +276,7 @@ public:
 
     void control_y_position(float er_y) {
         float u;
-        if (abs(er_y) > central_pixel_y * GSD / 1000) {
+        if (abs(er_y) > central_pixel_y * GSD) {
             u = u_k_y;
         } else {
             u = (-0.15 * (er_k_y - 0.75 * er_y) + u_k_y);
